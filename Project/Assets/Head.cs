b0e489ee82e9e53c30a44d119d384589d0ad5fc9
GameDevGeek <yannickassert@gmail.com>
2014-04-26 20:21:49 +0200

    Project added

---
+++ Project/Assets/Head.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Head : MonoBehaviour {
+
+	bool grounded;
+	
+	public LayerMask mask;
+	
+	public Vector2 dir;
+	
+	// Use this for initialization
+	void Start () {
+		dir = transform.right;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+//		rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * 10.0f, rigidbody2D.velocity.y);
+		
+		rigidbody2D.AddForce(dir * Input.GetAxis("Horizontal") * 20.0f);
+		
+		if(Input.GetKeyDown(KeyCode.Space) && grounded){
+			rigidbody2D.AddForce(transform.up * 500f);
+		}
+		
+		grounded = Physics2D.OverlapCircle(transform.position, 0.45f, mask);
+		
+		transform.rotation = Quaternion.Euler(Vector3.zero); //Lock rotation
+	}
+	
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.layer == 8){
+			//dir = col.gameObject.transform.right;	
+		}
+
+	}
+	
+	void OnCollisionStay2D(Collision2D col){
+		
+	}
+}
---
+++ Project/Assets/Segment.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Segment : MonoBehaviour {
+
+	public Transform successor;
+	bool grounded;
+	public LayerMask mask;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		Vector2 gap = successor.position - transform.position;
+		float dis = Vector2.Distance(transform.position, successor.position);
+		
+		grounded = Physics2D.OverlapCircle(transform.position, 0.45f, mask);
+		
+		if(dis > 0.85f){
+			rigidbody2D.velocity = gap * 10f * gap.sqrMagnitude;
+		}else{
+			if(grounded){
+				rigidbody2D.velocity = Vector2.zero;	
+			}else{
+				rigidbody2D.velocity = (Physics2D.gravity * 20f * Time.deltaTime);
+				
+			}
+		}
+		
+		transform.rotation = Quaternion.Euler(Vector3.zero); //Lock rotation
+		
+	}
+}
