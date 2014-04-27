70fa0c878fca0fc64b06cbfea55b1daa02e045f0
GameDevGeek <yannickassert@gmail.com>
2014-04-27 12:07:10 +0200

    gb works now, really

---
+++ Project/Assets/Scripts/GameBorder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBorder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerExit2D (Collider2D collider)
+    {
+        Destroy(collider.gameObject);
+    }
+}
