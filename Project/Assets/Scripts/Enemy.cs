a146d448ed0149b679acda5debfcf797dee04b80
GameDevGeek <yannickassert@gmail.com>
2014-05-10 23:01:02 +0200

    added some key features (see description)
    fruit collectible, enemy moves, moving platforms

---
+++ Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour 
+{
+    public bool moveLeft;
+    public float speed;
+
+	// Use this for initialization
+	void Start () 
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 movement;
+
+	    if (moveLeft)
+        {
+            movement = new Vector3(-Time.deltaTime*speed, 0.0f, 0.0f);
+        }
+        else
+        {
+            movement = new Vector3(Time.deltaTime*speed, 0.0f, 0.0f);
+        }
+
+        transform.position += movement;
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "EnemyBorder")
+        {
+            moveLeft = !moveLeft;
+        }
+        else if (other.tag == "Head" || other.tag == "Segment")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Project/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour 
+{
+    public bool moveLeft, leftPlatform;
+    public float speed, midPosX;
+    private float startX;
+
+	// Use this for initialization
+	void Start () 
+    {
+        startX = transform.position.x;
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        Vector3 movement;
+
+        if (moveLeft)
+        {
+            movement = new Vector3(-Time.deltaTime*speed, 0.0f, 0.0f);
+        }
+        else
+        {
+            movement = new Vector3(Time.deltaTime*speed, 0.0f, 0.0f);
+        }
+
+        transform.position += movement;
+
+        if (leftPlatform)
+        {
+            if ((transform.position.x < startX) || (transform.position.x > midPosX))
+            {
+                moveLeft = !moveLeft;
+            }
+        }
+        else
+        {
+            if ((transform.position.x > startX) || (transform.position.x < midPosX))
+            {
+                moveLeft = !moveLeft;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "MovingGroundSegment")
+        {
+            moveLeft = !moveLeft;
+        }
+    }
+}
