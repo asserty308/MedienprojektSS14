cf6b5692285ee061b9456c61489f209a2877b7d0
GameDevGeek <yannickassert@gmail.com>
2014-06-30 14:57:31 +0200

    The mole learned to chase the player

---
+++ Project/Assets/Scripts/Mole.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mole : MonoBehaviour 
+{
+    public GameObject moleActivator;
+    public float speedDamp = 0.15f;
+
+    private bool activated;
+    private Vector3 moveVector;
+
+	void Start () 
+    {
+        activated = false;
+        moveVector = new Vector3(1f, 0f, 0f);
+	}
+	
+	void Update () 
+    {
+	    if (activated)
+        {
+            transform.position += speedDamp * moveVector;
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Collision MoleActivator");
+        if (collider.tag == "Head" || collider.tag == "Segment")
+        {
+            activated = true;
+        }
+    }
+}
