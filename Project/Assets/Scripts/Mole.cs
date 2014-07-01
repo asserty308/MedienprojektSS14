88fd680e11c56a0d499cd9d8b411d5cee591d099
GameDevGeek <yannickassert@gmail.com>
2014-07-01 11:40:19 +0200

    improved mole behavior

--- Project/Assets/Scripts/Mole.cs
+++ Project/Assets/Scripts/Mole.cs
@@ -3,32 +3,87 @@ using System.Collections;
 
 public class Mole : MonoBehaviour 
 {
-    public GameObject moleActivator;
-    public float speedDamp = 0.15f;
+    public float speedFactor = 2f;
+    public float knockbackForce;
 
     private bool activated;
     private Vector3 moveVector;
+    private float startPositionX;
 
 	void Start () 
     {
         activated = false;
-        moveVector = new Vector3(1f, 0f, 0f);
+        moveVector = new Vector3(1f, moleMoveFunction(), 0f);
+        startPositionX = transform.position.x;
 	}
 	
 	void Update () 
     {
 	    if (activated)
         {
-            transform.position += speedDamp * moveVector;
+            transform.position += Time.deltaTime * moveVector;
+            moveVector = new Vector3(speedFactor, moleMoveFunction(), 0f);
         }
 	}
 
-    void OnTriggerEnter2D(Collider2D collider)
+    float moleMoveFunction()
     {
-        Debug.Log("Collision MoleActivator");
-        if (collider.tag == "Head" || collider.tag == "Segment")
+        float dx = transform.position.x - startPositionX;
+        return Mathf.Sin(dx);
+    }
+
+    public void setActivated(bool state)
+    {
+        activated = state;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Head")
         {
-            activated = true;
+            Head head = other.GetComponent<Head>();
+            Segment segment = head.successor != null ? head.successor : null;
+
+            if (segment == null)
+            {
+                CheckpointManager checkpointManager = other.GetComponent<CheckpointManager>();
+                GameObject currentCheckpointObject = checkpointManager.currentCheckpoint;
+                Checkpoint currentCheckpoint = currentCheckpointObject.GetComponent<Checkpoint>();
+
+                Destroy(head.gameObject);
+                currentCheckpoint.respawn();
+                return;
+            }
+
+            while (segment.successor != null)
+            {
+                segment = segment.successor;
+            }
+
+            Destroy(segment.gameObject);
+
+            //Little Knockback on impact
+            Vector3 knockbackDirection = head.facingRight ? -(other.transform.right) : other.transform.right;
+            other.gameObject.rigidbody2D.velocity = knockbackDirection * knockbackForce + new Vector3(0f, 10f, 0);
+            //
+
+        }
+        else if (other.tag == "Segment")
+        {
+            Segment segment = other.GetComponent<Segment>();
+
+            while (segment.successor != null)
+            {
+                segment = segment.successor;
+            }
+
+            Destroy(segment.gameObject);
+
+            //Little Knockback on impact
+            Head head = segment.head;
+            Vector3 knockbackDirection = head.facingRight ? other.transform.right : -other.transform.right;
+            head.gameObject.rigidbody2D.velocity = knockbackDirection * knockbackForce + new Vector3(0f, 10f, 0);
+            //
         }
     }
 }
---
+++ Project/Assets/Scripts/MoleActivator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoleActivator : MonoBehaviour 
+{
+    public Mole mole;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Collision MoleActivator");
+        if (collider.tag == "Head" || collider.tag == "Segment")
+        {
+            mole.setActivated(true);
+        }
+    }
+}
