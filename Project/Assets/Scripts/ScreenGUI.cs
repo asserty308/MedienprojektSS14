53ed0620aa73d135bd0fbe1710f48403da83d320
GameDevGeek <yannickassert@gmail.com>
2014-07-15 11:57:39 +0200

    Improved GUI-Style and other fixes
    also sorted the game objects view

--- Project/Assets/Scripts/ScreenGUI.cs
+++ Project/Assets/Scripts/ScreenGUI.cs
@@ -3,29 +3,31 @@ using System.Collections;
 
 public class ScreenGUI : MonoBehaviour 
 {
-    private string m_currentLabel;
+    private string m_currentLabel, m_scoreLabel;
     private bool m_showLabel;
-    //private Font myFont;
+    private GUIStyle m_style;
+    private PlayerScore m_playerScore;
 
     void Start()
     {
-        //myFont = (Font)Resources.Load("Fonts/Cabin_Sketch/CabinSketch-Bold", typeof(Font));
+        m_playerScore = GetComponent<PlayerScore>();
+        m_style = new GUIStyle();
+        m_style.font = (Font)Resources.Load("Fonts/Cabin_Sketch/CabinSketch-Bold");
+        m_style.normal.textColor = Color.blue;
         m_showLabel = true;
         m_currentLabel = GUILabels.moveString;
+        m_scoreLabel = m_playerScore.getGUIScore();
 
         Invoke("ToggleLabel", 10f);
     }
 
 	void OnGUI()
     {
-        /*GUIStyle mylabelStyle = new GUIStyle(GUI.skin.label);
-        mylabelStyle.fontSize = 250;
-
-        mylabelStyle.font = myFont;*/
+        GUI.Label(new Rect(10f, 10f, m_scoreLabel.Length * 3.5f, 20f), m_scoreLabel, m_style);
 
         if (m_showLabel)
         {
-            GUI.Label(new Rect((Screen.width >> 1) - m_currentLabel.Length * 3f, Screen.height >> 3, m_currentLabel.Length * 6f, 20f), m_currentLabel/*, mylabelStyle*/);
+           GUI.Label(new Rect((Screen.width >> 1) - m_currentLabel.Length * 3.5f, Screen.height >> 3, m_currentLabel.Length * 7f, 20f), m_currentLabel, m_style);
         }
     }
 
@@ -33,10 +35,15 @@ public class ScreenGUI : MonoBehaviour
     {
         m_showLabel = false;
     }
+
+    public void updateScoreLabel(int score)
+    {
+        m_scoreLabel = "Score: " + score;
+    }
 }
 
 public struct GUILabels
 {
-    public static string moveString = "You can move with arrow keys and jump with space";
+    public static string moveString = "Use the arrow keys to move to the left or right and the spacebar to jump";
 
 }
