e05205bf3a754eb4a213bb909e3fee5f89728d61
Hugo <iMACoperator@hugo.fritz.box>
2014-08-21 17:46:02 +0200

    Bugfixes
    session

--- Project/Assets/Scripts/Cherry.cs
+++ Project/Assets/Scripts/Cherry.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Cherry : MonoBehaviour {
 
 	public PlayerScore scoreKeeper;
+	public bool m_eaten;
 
 	// Use this for initialization
 	void Start () {
@@ -12,15 +13,20 @@ public class Cherry : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (m_eaten && !audio.isPlaying)
+		{
+			Destroy(this.gameObject);
+		}
+
 	}
 	
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Head")
 		{
+			audio.Play();
 			scoreKeeper.cherryEaten();
-			Destroy(this.gameObject);
-		}
+			gameObject.renderer.enabled = false;
+			m_eaten = true;		}
 	}
 }
--- Project/Assets/Scripts/ScreenGUI.cs
+++ Project/Assets/Scripts/ScreenGUI.cs
@@ -5,16 +5,25 @@ public class ScreenGUI : MonoBehaviour
 {
     private string m_currentLabel, m_scoreLabel;
     private bool m_showLabel;
-    private GUIStyle m_style;
+	private GUIStyle m_currentLabelStyle, m_scoreLabelStyle;
     private PlayerScore m_playerScore;
 
     void Start()
     {
+		Font font = (Font)Resources.Load("Fonts/Cabin_Sketch/CabinSketch-Bold");
+
         m_playerScore = GetComponent<PlayerScore>();
-        m_style = new GUIStyle();
-        m_style.font = (Font)Resources.Load("Fonts/Cabin_Sketch/CabinSketch-Bold");
-        m_style.normal.textColor = Color.blue;
-        m_showLabel = true;
+		m_currentLabelStyle = new GUIStyle();
+		m_currentLabelStyle.font = font;
+		m_currentLabelStyle.normal.textColor = Color.blue;
+		m_currentLabelStyle.fontSize = 48;
+
+		m_scoreLabelStyle = new GUIStyle();
+		m_scoreLabelStyle.font = font;
+		m_scoreLabelStyle.normal.textColor = Color.black;
+		m_scoreLabelStyle.fontSize = 64;
+
+		m_showLabel = true;
         m_currentLabel = GUILabels.moveString;
         m_scoreLabel = m_playerScore.getGUIScore();
 
@@ -23,11 +32,11 @@ public class ScreenGUI : MonoBehaviour
 
 	void OnGUI()
     {
-        GUI.Label(new Rect(10f, 10f, m_scoreLabel.Length * 3.5f, 20f), m_scoreLabel, m_style);
+        GUI.Label(new Rect(20f, 10f, m_scoreLabel.Length * 3.5f, 20f), m_scoreLabel, m_scoreLabelStyle);
 
         if (m_showLabel)
         {
-           GUI.Label(new Rect((Screen.width >> 1) - m_currentLabel.Length * 3.5f, Screen.height >> 3, m_currentLabel.Length * 7f, 20f), m_currentLabel, m_style);
+           GUI.Label(new Rect((Screen.width >> 1) - m_currentLabel.Length * 9.5f, Screen.height >> 2, m_currentLabel.Length * 7f, 20f), m_currentLabel, m_currentLabelStyle);
         }
     }
 
